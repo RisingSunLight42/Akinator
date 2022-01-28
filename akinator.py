569bcf5e4ea05639db0cdc60d34e3c42c5b037c5
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 16:03:52 +0100

    Ajout de la structure à l'intérieur du while, suivant les réponses de l'utilisateur, le jeu prend le chemin correspondant dans l'arbre

--- akinator.py
+++ akinator.py
@@ -10,4 +10,10 @@ monArbre = ["vit-il dans la maison ?", # Racine de départ
 def jeu1(arbre):
     print("Pense à un animal.")
     while not func_prim.estFeuille(arbre):
-        reponse = input(func_prim.racine(arbre))
+        reponse = input(f"${func_prim.racine(arbre)} ")
+        if reponse.lower().strip() == "oui":
+            arbre = func_prim.filsGauche(arbre)
+        elif reponse.lower().strip() == "non":
+            arbre = func_prim.filsDroit(arbre)
+        else:
+            print("La réponse entrée est invalide, répond bien par 'oui' ou 'non' !")
