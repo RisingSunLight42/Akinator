f12030e83931679d3ea524cecc2bba9abc0de1da
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 17:44:27 +0100

    Ajout du bouton jouer qui lancera la partie et affichera les boutons oui/non tout en se retirant

--- akinator_interface_graphique.py
+++ akinator_interface_graphique.py
@@ -1,7 +1,6 @@
 from sqlite3 import Row
 from tkinter import *
 
-
 # Création de la fenêtre
 fenetre = Tk()  # Crée la fenêtre de jeu
 fenetre.title("Akinator")  # Ajoute un titre à la fenêtre
@@ -15,10 +14,16 @@ variable_texte.set("Bienvenue sur le jeu Akinator ! Clique sur le bouton \"Jouer
 label_question.grid(row=0, column=0, columnspan=2)
 
 # Boutons
-bouton_oui = Button(fenetre, text="oui", command= lambda: variable_texte.set("Oui"))
-bouton_non = Button(fenetre, text="non", command= lambda: variable_texte.set("Non"))
 
-bouton_oui.grid(row=1, column=0)
-bouton_non.grid(row=1, column=1)
+def jeu():
+    bouton_oui = Button(fenetre, text="oui", command= lambda: variable_texte.set("Oui"))
+    bouton_non = Button(fenetre, text="non", command= lambda: variable_texte.set("Non"))
+    bouton_oui.grid(row=1, column=0)
+    bouton_non.grid(row=1, column=1)
+    bouton_jouer.grid_remove()
+    variable_texte.set("Le jeu peut commencer !")
+
+bouton_jouer = Button(fenetre, text="Jouer", command= lambda: jeu())
+bouton_jouer.grid(row=1, column=0, columnspan=2)
 
 fenetre.mainloop()  # Lance la fenêtre
