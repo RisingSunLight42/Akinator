cc8cb7df9a6b63fe458dc74b684b8fe2ed036c38
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 16:43:54 +0100

    Déplacement de la section de code pour rejouer, pour permettre de recommencer une partie même en cas de victoire d'Akinator

--- akinator.py
+++ akinator.py
@@ -44,20 +44,17 @@ def jeu2(arbre):
     resultat = "0"          # Crée une variable résultat avec une valeur arbitraire
     while resultat == "0":  # Tant que résultat a cette valeur arbitraire, on demande si le résultat est bon
         resultat = input(f"C'est un {arbre} ! J'ai raison ? ")  # Donne la réponse qu'il a trouvé et demande s'il a bon
-        if resultat.lower().strip() == "oui":
+        if resultat.lower().strip() == "oui":                   # Si Akinator a raison, dit qu'il a gagné
             print("Super ! J'ai encore gagné !")
         elif resultat.lower().strip() == "non":
             animal_attendu = input("C'était quoi alors ? ")
             question_proposee = input(f"Donne une question : oui pour {arbre}, non pour {animal_attendu} : ")
-            nouvel_arbre = func_prim.ajoute(question_proposee, arbre, animal_attendu, sauvegarde_arbre)  # Ajoute la question à l'arbre et stocke le nouvel arbre
-            rejoue = input("Veux-tu rejouer ? ")
-            if rejoue.lower().strip() == "oui":
-                jeu2(nouvel_arbre)
-            else:
-                print("Merci d'avoir joué ! À bientôt !")
+            sauvegarde_arbre = func_prim.ajoute(question_proposee, arbre, animal_attendu, sauvegarde_arbre)  # Ajoute la question à l'arbre et stocke le nouvel arbre
         else:
             resultat = "0"
             print("La réponse entrée est invalide, répond bien par 'oui' ou 'non' !")
-
-
-jeu2(monArbre)
+        rejoue = input("Veux-tu rejouer ? ")
+        if rejoue.lower().strip() == "oui":
+            jeu2(sauvegarde_arbre)
+        else:
+            print("Merci d'avoir joué ! À bientôt !")
