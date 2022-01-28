fa0148cc1b29207367cbdde5ae6b1f6b0bd1f9f2
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 15:56:31 +0100

    Ajout de l'import des fonctions primaire + ajout du paramètre arbre à la fonction jeu1 + début de la boucle while

--- akinator.py
+++ akinator.py
@@ -1,6 +1,13 @@
+from fonctions_primaires import estFeuille, racine
+
+
+import fonctions_primaires as func_prim
+
 monArbre = ["vit-il dans la maison ?", # Racine de départ
             ["a-t-il des poils ?", ["miaule-t-il ?", "Chat", "Hamster"], "Poisson rouge"],  # Arbre Gauche
             ["est-il dangereux ?", ["vole-t-il ?", "Frelon", "Tigre"], "Herisson"]]  # Arbre Droit
 
-def jeu1():
+def jeu1(arbre):
     print("Pense à un animal.")
+    while not func_prim.estFeuille(arbre):
+        reponse = input(func_prim.racine(arbre))
