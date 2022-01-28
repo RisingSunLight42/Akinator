3ad04a8b395badb1d116026ab9f7af304c9914aa
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 16:19:10 +0100

    Création de la fonction jeu2, qui reprend le code de jeu1

--- akinator.py
+++ akinator.py
@@ -23,3 +23,21 @@ def jeu1(arbre):
         else:                                           # Si la réponse est incorrecte, il donne un message d'erreur et repose la question
             print("La réponse entrée est invalide, répond bien par 'oui' ou 'non' !")
     print(f"C'est un {arbre} !")                        # Donne la réponse qu'il a trouvé
+
+
+def jeu2(arbre):
+    """Fonction simulant le déroulé d'un Akinator, mais avec en plus un système d'apprentissage
+
+    Args:
+        arbre (list): Arbre binaire contenant les questions qu'Akinator posera, ainsi que les réponses finales.
+    """
+    print("Pense à un animal.")                         # Demande au joueur de penser à l'animal qu'il souhaite faire deviner
+    while not func_prim.estFeuille(arbre):              # Tant que l'arbre n'est pas une feuille (donc un animal)
+        reponse = input(f"{func_prim.racine(arbre)} ")  # Il pose au joueur la question
+        if reponse.lower().strip() == "oui":            # Si elle est oui, l'arbre devient le filsGauche de l'arbre
+            arbre = func_prim.filsGauche(arbre)
+        elif reponse.lower().strip() == "non":          # Si elle est non, l'arbre devient le filsDroit de l'arbre
+            arbre = func_prim.filsDroit(arbre)
+        else:                                           # Si la réponse est incorrecte, il donne un message d'erreur et repose la question
+            print("La réponse entrée est invalide, répond bien par 'oui' ou 'non' !")
+    print(f"C'est un {arbre} !")                        # Donne la réponse qu'il a trouvé
