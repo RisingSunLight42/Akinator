78b8b06085790ff69e2a4cb167ca0290639356d8
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 18:03:45 +0100

    Création d'un objet Akinator pour permettre des modifications plus simples

--- akinator_interface_graphique.py
+++ akinator_interface_graphique.py
@@ -3,38 +3,43 @@ from time import sleep
 from fonctions_primaires import *
 from tkinter import *
 
-# Récupération de l'arbre
-with open("arbre_akinator.pik", "rb") as fichierArbre:
-        arbre = pickle.load(fichierArbre)
+class Akinator():
+    
+    def __init__(self):
+        with open("arbre_akinator.pik", "rb") as fichierArbre:
+                self.arbre = pickle.load(fichierArbre)
+        self.fenetre = Tk()  # Crée la fenêtre de jeu
+        self.variable_texte = StringVar()
+        self.creation_fenetre()
+    
+    def creation_fenetre(self):
+        self.fenetre.title("Akinator")  # Ajoute un titre à la fenêtre
+        self.fenetre.geometry("")  # Définit la taile de la fenêtre
 
-# Création de la fenêtre
-fenetre = Tk()  # Crée la fenêtre de jeu
-fenetre.title("Akinator")  # Ajoute un titre à la fenêtre
-fenetre.geometry("")  # Définit la taile de la fenêtre
+        # Label
+        label_question = Label(self.fenetre, textvariable=self.variable_texte)
 
-# Label
-variable_texte = StringVar()
-label_question = Label(fenetre, textvariable=variable_texte)
+        self.variable_texte.set("Bienvenue sur le jeu Akinator ! Clique sur le bouton \"Jouer\" pour commencer la partie !")
+        label_question.grid(row=0, column=0, columnspan=2)
 
-variable_texte.set("Bienvenue sur le jeu Akinator ! Clique sur le bouton \"Jouer\" pour commencer la partie !")
-label_question.grid(row=0, column=0, columnspan=2)
+        # Boutons
 
-# Boutons
+        def jeu():
+            """Lance le jeu en créant les boutons de réponse oui/non et modifiant la variable de texte pour la question
+            """
+            bouton_oui = Button(self.fenetre, text="oui", command= lambda: self.variable_texte.set("Oui"))
+            bouton_non = Button(self.fenetre, text="non", command= lambda: self.variable_texte.set("Non"))
+            bouton_oui.grid(row=1, column=0)
+            bouton_non.grid(row=1, column=1)
+            bouton_jouer.grid_remove()
+            self.variable_texte.set("Pense à un animal.")
+            self.fenetre.update()
+            sleep(3)
+            self.variable_texte.set(racine(self.arbre))
 
-def jeu():
-    """Lance le jeu en créant les boutons de réponse oui/non et modifiant la variable de texte pour la question
-    """
-    bouton_oui = Button(fenetre, text="oui", command= lambda: variable_texte.set("Oui"))
-    bouton_non = Button(fenetre, text="non", command= lambda: variable_texte.set("Non"))
-    bouton_oui.grid(row=1, column=0)
-    bouton_non.grid(row=1, column=1)
-    bouton_jouer.grid_remove()
-    variable_texte.set("Pense à un animal.")
-    fenetre.update()
-    sleep(3)
-    variable_texte.set(racine(arbre))
+        bouton_jouer = Button(self.fenetre, text="Jouer", command= lambda: jeu())
+        bouton_jouer.grid(row=1, column=0, columnspan=2)
 
-bouton_jouer = Button(fenetre, text="Jouer", command= lambda: jeu())
-bouton_jouer.grid(row=1, column=0, columnspan=2)
-
-fenetre.mainloop()  # Lance la fenêtre
+        self.fenetre.mainloop()  # Lance la fenêtre
+        
+partie = Akinator()
