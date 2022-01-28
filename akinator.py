2ea7e1cf452ed193ea2b134aaf0c128eac66e80e
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 16:10:52 +0100

    Ajout des commentaires et de la docstring

--- akinator.py
+++ akinator.py
@@ -8,16 +8,18 @@ monArbre = ["vit-il dans la maison ?", # Racine de départ
             ["est-il dangereux ?", ["vole-t-il ?", "Frelon", "Tigre"], "Herisson"]]  # Arbre Droit
 
 def jeu1(arbre):
-    print("Pense à un animal.")
-    while not func_prim.estFeuille(arbre):
-        reponse = input(f"{func_prim.racine(arbre)} ")
-        if reponse.lower().strip() == "oui":
+    """Fonction simulant le déroulé d'un Akinator
+
+    Args:
+        arbre (list): Arbre binaire contenant les questions qu'Akinator posera, ainsi que les réponses finales.
+    """
+    print("Pense à un animal.")                         # Demande au joueur de penser à l'animal qu'il souhaite faire deviner
+    while not func_prim.estFeuille(arbre):              # Tant que l'arbre n'est pas une feuille (donc un animal)
+        reponse = input(f"{func_prim.racine(arbre)} ")  # Il pose au joueur la question
+        if reponse.lower().strip() == "oui":            # Si elle est oui, l'arbre devient le filsGauche de l'arbre
             arbre = func_prim.filsGauche(arbre)
-        elif reponse.lower().strip() == "non":
+        elif reponse.lower().strip() == "non":          # Si elle est non, l'arbre devient le filsDroit de l'arbre
             arbre = func_prim.filsDroit(arbre)
-        else:
+        else:                                           # Si la réponse est incorrecte, il donne un message d'erreur et repose la question
             print("La réponse entrée est invalide, répond bien par 'oui' ou 'non' !")
-    print(f"C'est un {arbre} !")
-
-
-jeu1(monArbre)
+    print(f"C'est un {arbre} !")                        # Donne la réponse qu'il a trouvé
