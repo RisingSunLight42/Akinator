944900e6f2596cd1023a835d335d0f9209e52dd5
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 16:55:58 +0100

    Ajout de l'import de pickle + retrait de la liste monArbre, enregistrée maintenant dans le fichier binaire arbre_akinator.pik

--- akinator.py
+++ akinator.py
@@ -1,8 +1,6 @@
 import fonctions_primaires as func_prim
+import pickle
 
-monArbre = ["vit-il dans la maison ?", # Racine de départ
-            ["a-t-il des poils ?", ["miaule-t-il ?", "Chat", "Hamster"], "Poisson rouge"],  # Arbre Gauche
-            ["est-il dangereux ?", ["vole-t-il ?", "Frelon", "Tigre"], "Herisson"]]  # Arbre Droit
 
 def jeu1(arbre):
     """Fonction simulant le déroulé d'un Akinator
