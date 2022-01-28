39a0bcfc5c178b002d8684d5bb2e54030a8b5a8e
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 17:21:38 +0100

    Création de la fenêtre

---
+++ akinator_interface_graphique.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+# Création de la fenêtre
+fenetre = Tk()  # Crée la fenêtre de jeu
+fenetre.title("Akinator")  # Ajoute un titre à la fenêtre
+fenetre.configure(width=1000, height=1000)  # Définit la taile de la fenêtre
+
+
+fenetre.mainloop()  # Lance la fenêtre
