7e2ee52a0ffd90ce7762e26fd1780d04bbca3767
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 17:32:26 +0100

    Ajout des boutons oui et non, pour répondre aux questions

--- akinator_interface_graphique.py
+++ akinator_interface_graphique.py
@@ -13,4 +13,11 @@ label_question = Label(fenetre, textvariable=variable_texte)
 variable_texte.set("Heyo")
 label_question.pack()
 
+# Boutons
+bouton_oui = Button(fenetre, text="oui", command= lambda: variable_texte.set("Oui"))
+bouton_non = Button(fenetre, text="non", command= lambda: variable_texte.set("Non"))
+
+bouton_oui.pack()
+bouton_non.pack()
+
 fenetre.mainloop()  # Lance la fenêtre
