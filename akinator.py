c2d5e6a9108f1374d080abde9e0f1092a4e490cd
RisingSunLight <risingsunlight5776@gmail.com>
2022-01-28 15:52:02 +0100

    Ajout de la liste monArbre, contenant les questions de l'Akinator

--- akinator.py
+++ akinator.py
@@ -1,4 +1,6 @@
-"""Ici se trouvera les fonctions de jeu"""
+monArbre = ["vit-il dans la maison ?", # Racine de départ
+            ["a-t-il des poils ?", ["miaule-t-il ?", "Chat", "Hamster"], "Poisson rouge"],  # Arbre Gauche
+            ["est-il dangereux ?", ["vole-t-il ?", "Frelon", "Tigre"], "Herisson"]]  # Arbre Droit
 
 def jeu1():
     print("Pense à un animal.")
